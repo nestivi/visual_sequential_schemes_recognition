de1d048b09126465e79d06f6a75f8672bb1ed082
Nestivi <mkr.kul2003@gmail.com>
2025-11-17 15:30:14 +0100

    Adding fast startup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from visual schemes recognition!")
+import runpy
 
+def main():
+    runpy.run_module("vssr", run_name="__main__")
 
 if __name__ == "__main__":
     main()
