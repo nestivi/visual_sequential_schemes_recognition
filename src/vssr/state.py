45ec3e9f7473e1a906dbe953f5539ba3b2d1961e
Nestivi <mkr.kul2003@gmail.com>
2025-12-02 14:20:38 +0100

    feat: add duration of pressing button

--- src/vssr/state.py
+++ src/vssr/state.py
@@ -1,19 +1,23 @@
 from .config import BUTTONS
+import time
 
-# Sekwencja kliknięć
+# click sequence
 click_sequence = []
+button_press_times = {}
 
-# Stan przycisków
-button_states = {name: "podniesiony" for name in BUTTONS}
-
+# button states
+button_states = {name: "raised" for name in BUTTONS}
 def register_click(button_name):
-    """Rejestruje kliknięcie, jeśli przycisk jest w stanie 'podniesiony'."""
+    """Registers a click if the button is in the 'raised' state."""
     global click_sequence, button_states
-    if button_states[button_name] == "podniesiony":
-        click_sequence.append(button_name)
-        button_states[button_name] = "wcisniety"
-        print(f"WYKRYTO KLIKNIĘCIE: {button_name}")
+    if button_states[button_name] == "raised":
+        button_press_times[button_name] = time.time()
+        button_states[button_name] = "pressed"
+        print(f"CLICK DETECTED: {button_name}")
 
 def reset_button(button_name):
-    """Resetuje stan przycisku, gdy palec nie jest nad nim."""
-    button_states[button_name] = "podniesiony"
+    """Resets the button state when the finger is not over it."""
+    if button_states[button_name] == "pressed":
+        duration = time.time() - button_press_times[button_name]
+        click_sequence.append((button_name, duration))
+    button_states[button_name] = "raised"
